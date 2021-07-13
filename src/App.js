9a785a4b245ef7e383c5136ff0eb0c04d6dc1028
Pushpit <pushpit07@gmail.com>
2021-07-13 12:49:35 +0530

    useEffect done
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import Setup from "./tutorial/1-useState/setup/5-useState-counter";
-import Final from "./tutorial/1-useState/final/4-useState-object";
+import Setup from "./tutorial/2-useEffect/setup/1-useEffect-basics";
+import Final from "./tutorial/2-useEffect/final/3-useEffect-fetch-data";
 
 function App() {
 	return (
 		<div className="container">
 			<h1 style={{ marginBottom: "100px" }}>Advanced React</h1>
-			<Setup />
-			{/* <Final /> */}
+			{/* <Setup /> */}
+			<Final />
 		</div>
 	);
 }
